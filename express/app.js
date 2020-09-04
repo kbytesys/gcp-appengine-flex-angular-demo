62b7b0ffdc5bb18d1e17bef67158e981fa315e7c
Andrea Briganti <andrea.briganti@nttdata.com>
2020-09-04 15:13:24 +0200

    initial commit
    Signed-off-by: Andrea Briganti <andrea.briganti@nttdata.com>

---
+++ express/app.js
@@ -0,0 +1,22 @@
+"use strict";
+const express = require("express");
+const compression = require("compression");
+const probe = require('kube-probe');
+
+const _port = 8080;
+const _app_folder = 'dist';
+
+const app = express();
+probe(app);
+app.set('trust proxy', true);
+app.use(compression());
+
+app.get('*.*', express.static(_app_folder, {maxAge: '1y'}));
+
+app.all('*', function (req, res) {
+  res.status(200).sendFile(`/`, {root: _app_folder});
+});
+
+app.listen(_port, function () {
+  console.log("Node Express server for " + app.name + " listening on port " + _port);
+});
